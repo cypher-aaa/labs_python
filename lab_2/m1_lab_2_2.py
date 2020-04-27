93ec6a97494f07e2b74ea4a004104d1b39efe778
Aleksandr Alekseev <alekseev4321@gmail.com>
2020-04-27 19:43:57 +0300

    Upload Task 2

---
+++ lab_2/m1_lab_2_2.py
@@ -0,0 +1,156 @@
+import argparse
+from sys import stdout
+from random import choice, choices
+from string import ascii_letters
+
+
+def generate_word(l):
+    w_length = choice(range(l[0], l[1] + 1))
+    word = "".join(choices(ascii_letters, k=w_length))
+    return word
+
+
+def generate_line(k, l):
+    w_count = choice(range(k[0], k[1] + 1))
+    line = " ".join([generate_word(l) for x in range(w_count)])
+    return line
+
+
+def generate_file(name, size_mb, k, l):
+    print("Генерация файла...")
+    size_b = int(1024**2*size_mb)
+    f_size = 0
+    with open(name, "w+") as f:
+        while (f_size < size_b):
+            status_bar(f_size, size_b)
+            new_line = generate_line(k, l)
+            new_word = generate_word(l)
+            if (f_size + len(new_line) <= size_b):
+                f.write(new_line)
+                f_size += len(new_line)
+                if (f_size + 2 <= size_b):
+                    f.write("\n")
+                    f_size += 2
+            elif (f_size + len(new_word) <= size_b):
+                f.write(new_word)
+                f_size += len(new_word)
+                if (f_size + 1 <= size_b):
+                    f.write(" ")
+                    f_size += 1
+            else:
+                tail = size_b - f_size
+                if (tail in range(l[0], l[1] + 1)):
+                    new_word = generate_word((tail, tail))
+                    f.write(new_word)
+                    f_size += len(new_word)
+                else:
+                    f.write(" " * tail)
+                    f_size += tail
+        status_bar(f_size, size_b)
+    print("\n"+"All done!")
+
+
+def status_bar(current, final):
+    curr = int(current/(final/20))
+    status = '[' + '#'*curr + '-'*(20-curr) + ']'
+    stdout.write("\r" + str(status) + " " + str(current) + "/" + str(final) +
+                 " bytes")
+    stdout.flush()
+
+
+def check_name(name):
+    if (name.endswith(".txt")):
+        try:
+            f = open(name, "w")
+            f.close()
+            return name
+        except OSError:
+            print("Некорректное имя файла!")
+    else:
+        print("Имя файла должно заканчиваться на '.txt'!")
+    return None
+
+
+def check_size(size):
+    try:
+        size = float(size)
+        if (size > 0):
+            return size
+        else:
+            print("Размер файла должен быть положительным!")
+    except ValueError:
+        print("Некорректный ввод!")
+    return None
+
+
+def check_k(k):
+    try:
+        if (k):
+            k = tuple(map(int, k.split(',')))
+            if (k[0] <= k[1]):
+                return k
+            else:
+                print("Введите числа по порядку!")
+        else:
+            k = (10, 100)
+            return k
+    except ValueError:
+        print("Некорректный ввод!")
+    return None
+
+
+def check_l(l):
+    try:
+        if (l):
+            l = tuple(map(int, l.split(',')))
+            if (l[0] <= l[1]):
+                return l
+            else:
+                print("Введите числа по порядку!")
+        else:
+            l = (3, 10)
+            return l
+    except ValueError:
+        print("Некорректный ввод!")
+    return None
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", help="enter name")
+    parser.add_argument("--size", help="enter size")
+    parser.add_argument("--k", help="enter k (optional)")
+    parser.add_argument("--l", help="enter l (optional)")
+    args = parser.parse_args()
+    if args.name:
+        name = check_name(args.name)
+        size = check_size(args.size)
+        k = check_k(args.k)
+        l = check_l(args.l)
+        if (name and size and k and l):
+            generate_file(name, size, k, l)
+    else:
+        print("Введите имя файла (расширение - .txt):")
+        while(True):
+            name = check_name(input())
+            if(name):
+                break
+        print("Введите размер файла (Mb):")
+        while(True):
+            size = check_size(input())
+            if(size):
+                break
+        print("Введите k через запятую ('ввод' для значения по умолчанию):")
+        while(True):
+            k = check_k(input())
+            if(k):
+                break
+        print("Введите l через запятую ('ввод' для значения по умолчанию):")
+        while(True):
+            l = check_l(input())
+            if(l):
+                break
+        generate_file(name, size, k, l)
+
+if __name__ == "__main__":
+    main()
